020637263cc70055a8f44920611a42c2dad07935
anasbn44 <banouni.anas0@gmail.com>
2023-04-11 22:47:12 +0000

    commit-2

--- src/main/java/ma/enset/service/BankGrpcService.java
+++ src/main/java/ma/enset/service/BankGrpcService.java
@@ -4,6 +4,9 @@ import io.grpc.stub.StreamObserver;
 import ma.enset.stubs.Bank;
 import ma.enset.stubs.BankServiceGrpc;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
     @Override
     public void convert(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
@@ -22,16 +25,75 @@ public class BankGrpcService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
     public void getCurrencyStream(Bank.ConvertCurrencyRequest request, StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        super.getCurrencyStream(request, responseObserver);
+        String currencyFrom = request.getCurrencyFrom();
+        String currencyTo = request.getCurrencyTo();
+        double amount = request.getAmount();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            int counter = 0;
+            @Override
+            public void run() {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setCurrencyFrom(currencyFrom)
+                        .setCurrencyTo(currencyTo)
+                        .setAmount(amount)
+                        .setResult(amount * Math.random() * 100)
+                        .build();
+                responseObserver.onNext(response);
+                ++counter;
+                if (counter == 5){
+                    responseObserver.onCompleted();
+                    timer.cancel();
+                }
+            }
+        }, 1000, 1000);
     }
 
     @Override
     public StreamObserver<Bank.ConvertCurrencyRequest> performStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        return super.performStream(responseObserver);
+        return new StreamObserver<Bank.ConvertCurrencyRequest>() {
+            double sum = 0;
+            @Override
+            public void onNext(Bank.ConvertCurrencyRequest convertCurrencyRequest) {
+                sum +=convertCurrencyRequest.getAmount();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setResult(sum * 11.8)
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
     public StreamObserver<Bank.ConvertCurrencyRequest> fullStream(StreamObserver<Bank.ConvertCurrencyResponse> responseObserver) {
-        return super.fullStream(responseObserver);
+        return new StreamObserver<Bank.ConvertCurrencyRequest>() {
+            @Override
+            public void onNext(Bank.ConvertCurrencyRequest convertCurrencyRequest) {
+                Bank.ConvertCurrencyResponse response = Bank.ConvertCurrencyResponse.newBuilder()
+                        .setResult(convertCurrencyRequest.getAmount() * Math.random() * 10)
+                        .build();
+                responseObserver.onNext(response);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
